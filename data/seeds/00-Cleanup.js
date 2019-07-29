33cfa6662b57edecf0639b927127d0eaf4a5dab2
Winnie Song <winniesong0121@gmail.com>
2019-07-29 14:45:03 -0400

    added back in cleanup

--- data/seeds/00-Cleanup.js
+++ data/seeds/00-Cleanup.js
@@ -1,13 +1,7 @@
+const cleaner = require('knex-cleaner');
+// will prevent foreign key errors when trying to truncate the tables that have FK pointing to them
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+	// Deletes ALL existing entries
+	return cleaner.clean(knex);
 };
