caa4a3899bf843994b97d0bbc07a9d35e2aa455e
Winnie Song <winniesong0121@gmail.com>
2019-07-28 22:08:50 -0400

    test test test

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,11 @@ module.exports = {
     
     production: {
         client: "pg",
-        connection: process.env.DATABASE_URL,
+        connection: process.env.DATABASE_URL +'?ssl=true',
+        pool: {
+            min: 2,
+            max: 10
+        },
         migrations: {
             directory: "./data/migrations/"
         },
