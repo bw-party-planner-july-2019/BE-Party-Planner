0b5b47b24c6d0b93f5429cc668d0822b58551e1e
Winnie Song <winniesong0121@gmail.com>
2019-07-28 21:55:27 -0400

    changed seeds

--- data/models/party-model.test.js
+++ data/models/party-model.test.js
@@ -12,12 +12,12 @@ describe('Party Model', () => {
 
 	describe('add', () => {
 		it('add party into database', async () => {
-			const newParty = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			expect(newParty.party_name).toBe('Don promotion');
+			const newParty = await Party.add({ user_id: 1, party_name: 'B-day' });
+			expect(newParty.party_name).toBe('B-day');
 		});
 		it('adding 2 parties to the database', async () => {
-			const newParty1 = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const newParty2 = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
+			const newParty1 = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const newParty2 = await Party.add({ user_id: 1, party_name: 'X-mas' });
 			const allOfThem = await db('party');
 			expect(allOfThem).toHaveLength(2);
 		});
@@ -25,7 +25,7 @@ describe('Party Model', () => {
 
 	describe('find', () => {
 		it('find party in database', async () => {
-			const newParty = await Party.add({ user_id: 1, party_name: 'Don promotion' });
+			const newParty = await Party.add({ user_id: 1, party_name: 'B-day' });
 			const findParty = await Party.find();
 			expect(findParty).toEqual([
 				{
@@ -33,15 +33,15 @@ describe('Party Model', () => {
 					date: null,
 					id: 1,
 					n_of_guests: null,
-					party_name: 'Don promotion',
+					party_name: 'B-day',
 					theme: null,
 					user_id: 1
 				}
 			]);
 		});
 		it('find 2 parties after adding them to the database', async () => {
-			const newParty1 = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const newParty2 = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
+			const newParty1 = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const newParty2 = await Party.add({ user_id: 1, party_name: 'X-mas' });
 			const findParty = await Party.find();
 			expect(findParty).toEqual([
 				{
@@ -49,7 +49,7 @@ describe('Party Model', () => {
 					date: null,
 					id: 1,
 					n_of_guests: null,
-					party_name: 'Don promotion',
+					party_name: 'B-day',
 					theme: null,
 					user_id: 1
 				},
@@ -58,7 +58,7 @@ describe('Party Model', () => {
 					date: null,
 					id: 2,
 					n_of_guests: null,
-					party_name: 'Hawaiian',
+					party_name: 'X-mas',
 					theme: null,
 					user_id: 1
 				}
@@ -68,29 +68,29 @@ describe('Party Model', () => {
 
 	describe('findById', () => {
 		it('find by party id one party in a database with 2 parties', async () => {
-			const newParty1 = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const newParty2 = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
+			const newParty1 = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const newParty2 = await Party.add({ user_id: 1, party_name: 'X-mas' });
 			const findParty = await Party.findById(1);
 			expect(findParty).toEqual({
 				budget: null,
 				date: null,
 				id: 1,
 				n_of_guests: null,
-				party_name: 'Don promotion',
+				party_name: 'B-day',
 				theme: null,
 				user_id: 1
 			});
 		});
 		it('find by party id a different party in a database with 2 parties', async () => {
-			const newParty1 = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const newParty2 = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
+			const newParty1 = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const newParty2 = await Party.add({ user_id: 1, party_name: 'X-mas' });
 			const findParty = await Party.findById(2);
 			expect(findParty).toEqual({
 				budget: null,
 				date: null,
 				id: 2,
 				n_of_guests: null,
-				party_name: 'Hawaiian',
+				party_name: 'X-mas',
 				theme: null,
 				user_id: 1
 			});
@@ -99,16 +99,16 @@ describe('Party Model', () => {
 
 	describe('remove', () => {
 		it('removes party correctly', async () => {
-			const partyOne = await Party.add({ user_id: 1, party_name: 'Don promotion' });
+			const partyOne = await Party.add({ user_id: 1, party_name: 'B-day' });
 			const removeparty = await Party.remove(1);
 
 			const allOfThem = await db('party');
 			expect(allOfThem).toHaveLength(0);
 		});
 		it('removes party correctly when adding more than one', async () => {
-			const partyOne = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const partyTwo = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
-			const partyThree = await Party.add({ user_id: 1, party_name: 'party time' });
+			const partyOne = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const partyTwo = await Party.add({ user_id: 1, party_name: 'X-mas' });
+			const partyThree = await Party.add({ user_id: 1, party_name: 'party party' });
 			const removeparty = await Party.remove(1);
 
 			const allOfThem = await db('party');
@@ -118,15 +118,15 @@ describe('Party Model', () => {
 
 	describe('update', () => {
 		it('update 1 party in database', async () => {
-			const newParty1 = await Party.add({ user_id: 1, party_name: 'Don promotion' });
-			const newParty2 = await Party.add({ user_id: 1, party_name: 'Hawaiian' });
+			const newParty1 = await Party.add({ user_id: 1, party_name: 'B-day' });
+			const newParty2 = await Party.add({ user_id: 1, party_name: 'X-mas' });
 			const updateParty = await Party.update(1, {
 				budget: 500,
 				date: null,
 				id: 1,
 				n_of_guests: 20,
-				party_name: 'Don promotion',
-				theme: 'Hawaiian',
+				party_name: 'B-day',
+				theme: 'Golden',
 				user_id: 1
 			});
 			const findParty = await Party.findById(1);
@@ -135,10 +135,11 @@ describe('Party Model', () => {
 				date: null,
 				id: 1,
 				n_of_guests: 20,
-				party_name: 'Don promotion',
-				theme: 'Hawaiian',
+				party_name: 'B-day',
+				theme: 'Golden',
 				user_id: 1
 			});
 		});
 	});
 });
+//
--- data/seeds/05-Todo.js
+++ data/seeds/05-Todo.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
 	return knex('todo_list').del().then(function() {
 		return knex('todo_list').insert([
-			{ id: 1, party_id: 1, item: 'Rent venue', completed: false },
+			{ id: 1, party_id: 1, item: 'Rent costumes', completed: false },
 			{ id: 2, party_id: 1, item: 'Buy decorations', completed: false },
 			{ id: 3, party_id: 2, item: 'Finish DIYs', completed: false },
 			{ id: 4, party_id: 2, item: 'Clean up', completed: false },
