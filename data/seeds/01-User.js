4fa003b79d98de94f76d21e99f640dc440524405
Winnie Song <winniesong0121@gmail.com>
2019-07-29 14:16:27 -0400

    hashed passwords for production

--- data/seeds/01-User.js
+++ data/seeds/01-User.js
@@ -1,9 +1,12 @@
+const bcrypt = require('bcryptjs');
+
 exports.seed = function(knex, Promise) {
+	const password = bcrypt.hashSync('1234', 10);
 	return knex('users').del().then(function() {
 		return knex('users').insert([
-			{ id: 1, username: 'Ian', password: '1234' },
-			{ id: 2, username: 'Elisa', password: '1234' },
-			{ id: 3, username: 'Josh', password: '1234' }
+			{ id: 1, username: 'Ian', password: password },
+			{ id: 2, username: 'Elisa', password: password },
+			{ id: 3, username: 'Josh', password: password }
 		]);
 	});
 };
